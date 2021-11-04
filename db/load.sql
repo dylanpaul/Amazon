11bc1718d01dd717f790e84d64635cc18304d24d
Isabella Geraci <icg2@duke.edu>
2021-11-04 14:24:37 -0400

    newest version

--- db/load.sql
+++ db/load.sql
@@ -1,6 +1,6 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY Purchases FROM 'data/Seller.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY Purchases FROM 'data/Cart_Items.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY Purchases FROM 'data/Coupons.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Seller FROM 'data/Seller.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Cart_Items FROM 'data/Cart_Items.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Coupons FROM 'data/Coupons.csv' WITH DELIMITER ',' NULL '' CSV
